fbdd3e16aab21aab297b40e3b5547739187a93ca
Ryan Mahoney <ryan@virtuecenter.com>
2014-11-23 02:54:56 -0500

    working

--- src/SES.php
+++ src/SES.php
@@ -10,10 +10,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -30,11 +30,7 @@ class SES {
     private $client;
     private $message = false;
 
-    public function __construct ($config) {
-        $config = $config->amazonses;
-        if ($config === false) {
-            throw new \Exception('Missing amazonses configuration.');
-        }
+    public function __construct (Array $config) {
         $this->client = SesClient::factory((array)$config);
     }
 
